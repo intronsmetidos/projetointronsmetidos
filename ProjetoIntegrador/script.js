20ed06f27983dbde59d4d7643e88639c7c5f0252
intronsmetidos <123427229+intronsmetidos@users.noreply.github.com>
2023-02-03 09:16:03 -0300

    Update script.js

--- ProjetoIntegrador/script.js
+++ ProjetoIntegrador/script.js
@@ -1,25 +1,3 @@
-let glowInTexts = document.querySelectorAll(".description");
-
-glowInTexts.forEach(glowInText => {
-
-  let letters = glowInText.textContent.split("");
-
-  glowInText.textContent = "";
-
-  letters.forEach((letter, i) => {
-
-    let span = document.createElement("span");
-
-    span.textContent = letter;
-
-    span.style.animationDelay = `${i * 0.05}s`;
-
-    glowInText.append(span);
-
-  });
-
-});
-
 
 
 let slideIndex = 1;
