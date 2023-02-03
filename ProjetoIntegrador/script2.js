1c9f55e10a51d451bebc52ea09c10ca87ac7b418
intronsmetidos <123427229+intronsmetidos@users.noreply.github.com>
2023-02-03 11:10:03 -0300

    Update script2.js

--- ProjetoIntegrador/script2.js
+++ ProjetoIntegrador/script2.js
@@ -3,7 +3,7 @@ function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 750 * i);
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
     });
   }
 
