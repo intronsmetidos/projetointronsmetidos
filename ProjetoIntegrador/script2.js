1fe90d0412a5cad2b502e226ea32b3ee025f64ff
intronsmetidos <123427229+intronsmetidos@users.noreply.github.com>
2023-02-03 10:52:54 -0300

    Update script2.js

--- ProjetoIntegrador/script2.js
+++ ProjetoIntegrador/script2.js
@@ -23,3 +23,14 @@
   const titulo = document.querySelector('.description');
   typeWriter(titulo);
 </script>
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+
+
+  const titulo = document.querySelector('.description');
+  typeWriter(titulo);
