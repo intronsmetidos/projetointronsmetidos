e9979f0fe85e4bb88c2cdacc8dd8b56fb90eec62
intronsmetidos <123427229+intronsmetidos@users.noreply.github.com>
2023-02-05 04:29:18 -0300

    Update zoom.js

--- ProjetoIntegrador/nucleo/zoom.js
+++ ProjetoIntegrador/nucleo/zoom.js
@@ -1,8 +1,12 @@
 function aumentaImagem(nome){
-	nome.width = nome.width+50;
-	nome.height = nome.height+50;			
-     }
-     function tamanhoNormal(nome){
-	nome.width = 150;
-	nome.height = 30;			
-     }
+if (nome.height = 50%){
+return;
+}
+nome.width = nome.width+50;
+nome.height = nome.height+50;
+}
+
+function tamanhoNormal(nome){
+nome.width = 50%;
+nome.height = 50%;
+}
